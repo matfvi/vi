8747e5ade5b90f6894c6f03762d2cd849399074e
Marko Spasic <spaskeasm@gmail.com>
2021-04-09 13:08:11 +0200

    Update logic.py

--- 2020.2021/06_Logika_Uvod/logic.py
+++ 2020.2021/06_Logika_Uvod/logic.py
@@ -1,184 +1,190 @@
 import copy
 from itertools import combinations
-# x y z q
-# 
 
+# x,y,z,w # 2 ^ n
 def all_valuations(variables):
+    # r=0,1,2,3,,,len(variables)
     for r in range(len(variables) + 1):
-        for true_vars in combinations(variables, r):
-            result = {x:False for x in variables}
-            result.update({x: True for x in true_vars})
+        for true_varibles in combinations(variables, r):
+            result = {x: False for x in variables}
+            result.update({x:True for x in true_varibles})
             yield result
-
-
 
 class Formula:
     def __init__(self):
         self.components = []
 
-    def __repr__(self):
-        return str(self)
-
     def interpret(self, valuation):
         pass
 
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, rhs): # self == rhs => self.__eq__(rhs)
+        return Eq(self.copy(), rhs.copy())
+
     def __and__(self, rhs): # self & rhs => self.__and__(rhs)
-        return And([self.copy(), rhs.copy()])
-    
-    def __or__(self, rhs): # self | rhs
-        return Or([self.copy(), rhs.copy()])
+        return And(self.copy(), rhs.copy())
 
-    def __invert__(self): # ~self
-        return Not([self.copy()])
-
-    def __eq__(self, rhs):
-        return Eq([self.copy(), rhs.copy()])
+    def __or__(self, rhs):
+        return Or(self.copy(), rhs.copy())
 
     def __rshift__(self, rhs):
-        return Impl([self.copy(), rhs.copy()])
+        return Impl(self.copy(), rhs.copy())
 
+    def __invert__(self):
+        return Not(self.copy())
+    
     def copy(self):
-        return copy.deepcopy(self) 
+        return copy.deepcopy(self)
 
-    def get_all_varaibles(self):
+    def get_all_variables(self):
         result = set()
         for c in self.components:
-            result.update(c.get_all_varaibles())
+            result.update(c.get_all_variables())
         return result
 
-    def is_tautology(self):
-        variables = list(self.get_all_varaibles())
+    def is_valid(self):
+        variables = list(self.get_all_variables())
         for valuation in all_valuations(variables):
             if self.interpret(valuation) == False:
-                return (False, valuation)
-        return (True, None)
+                return False, valuation
+        return True, None
 
     def is_satisfiable(self):
-        variables = list(self.get_all_varaibles())
+        variables = list(self.get_all_variables())
         for valuation in all_valuations(variables):
             if self.interpret(valuation) == True:
                 return True, valuation
         return False, None
-    
-    def is_valid(self):
-        return self.is_tautology()
-    
-    def is_unsatisfiable(self):
-        variables = list(self.get_all_varaibles())
+
+    def is_contradictory(self):
+        variables = list(self.get_all_variables())
         for valuation in all_valuations(variables):
             if self.interpret(valuation) == True:
                 return False, valuation
         return True, None
-    
+
     def is_falsifiable(self):
-        variables = list(self.get_all_varaibles())
+        variables = list(self.get_all_variables())
         for valuation in all_valuations(variables):
             if self.interpret(valuation) == False:
                 return True, valuation
         return False, None
 
+    def all_valuations_that_interpret_to_true(self):
+        result = []
+        variables = list(self.get_all_variables())
+        for valuation in all_valuations(variables):
+            if self.interpret(valuation) == True:
+                result.append(valuation)
+        return result
 
+    
+class Var(Formula):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def interpret(self, valuation):
+        return valuation[self.name]
+
+    def get_all_variables(self):
+        return set([self.name]) # prom => {'p', 'r', 'o', 'm'}
+
+    def __str__(self):
+        return self.name
 
 class Const(Formula):
     def __init__(self, value):
         super().__init__()
         self.value = value
-
+    
     def interpret(self, valuation):
         return self.value
-    
-    def get_all_varaibles(self):
-        return set()
 
     def __str__(self):
-        return "{}".format(1 if self.value == True else 0)
+        return "{}".format(1 if self.value else 0)
 
 class And(Formula):
-    def __init__(self, components):
+    def __init__(self, lhs, rhs):
         super().__init__()
-        self.components = components # lhs == components[0], rhs == components[1]
-    
+        self.components = [lhs, rhs]
+
     def interpret(self, valuation):
         return self.components[0].interpret(valuation) and self.components[1].interpret(valuation)
 
     def __str__(self):
-        return f"({str(self.components[0])}) & ({str(self.components[1])})"
+        return f"({self.components[0]}) & ({self.components[1]})"
+
 
 class Or(Formula):
-    def __init__(self, components):
+    def __init__(self, lhs, rhs):
         super().__init__()
-        self.components = components # lhs == components[0], rhs == components[1]
+        self.components = [lhs, rhs]
 
     def interpret(self, valuation):
         return self.components[0].interpret(valuation) or self.components[1].interpret(valuation)
 
     def __str__(self):
-        return f"({str(self.components[0])}) | ({str(self.components[1])})"
+        return f"({self.components[0]}) | ({self.components[1]})"
+
 
 class Impl(Formula):
-    def __init__(self, components):
+    def __init__(self, lhs, rhs):
         super().__init__()
-        self.components = components # lhs == components[0], rhs == components[1]
+        self.components = [lhs, rhs]
 
     def interpret(self, valuation):
-        # A => B <=> ~A | B
+        # p => q <=> not p ili q
         return not self.components[0].interpret(valuation) or self.components[1].interpret(valuation)
 
     def __str__(self):
-        return f"({str(self.components[0])}) >> ({str(self.components[1])})"
+        return f"({self.components[0]}) >> ({self.components[1]})"
 
 class Eq(Formula):
-    def __init__(self, components):
+    def __init__(self, lhs, rhs):
         super().__init__()
-        self.components = components # lhs == components[0], rhs == components[1]
-
+        self.components = [lhs, rhs]
+    
     def interpret(self, valuation):
         return self.components[0].interpret(valuation) == self.components[1].interpret(valuation)
 
     def __str__(self):
-        return f"({str(self.components[0])}) == ({str(self.components[1])})"
+        return f"({self.components[0]}) == ({self.components[1]})"
 
 class Not(Formula):
-    def __init__(self, components):
+    def __init__(self, op):
         super().__init__()
-        self.components = components # lhs == components[0]
+        self.components = [op]
 
     def interpret(self, valuation):
         return not self.components[0].interpret(valuation)
 
     def __str__(self):
-        return f"~({str(self.components[0])})"
-
-class Var(Formula):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-    
-    def interpret(self, valuation):
-        return valuation[self.name]
-
-    def get_all_varaibles(self):
-        return set([self.name])
-
-    def __str__(self):
-        return self.name
+        return f"~({self.components[0]})"
 
 def vars(names):
-    names = [name.strip() for name in names.split(',')]
-    return [Var(name) for name in names]
+    return [Var(name.strip()) for name in names.split(',')]
 
 def primer_1():
-    p,q,w,r,t = vars("p,q,w,r,t")
-    formula = (p >> q) | ((w == r) & (r & ~t))
+    x, y, z = vars("x,y,z")
+    formula = x | ~x
+    # formula = Or(
+    #     And(x,y),
+    #     Impl(y,z)
+    # )
     valuation = {
-        "p": True,
-        "q": False,
-        "w": True,
-        "r": True,
-        "t": False
+        "x": True,
+        "y": False,
+        "z": True
     }
     print(formula)
     print(formula.interpret(valuation))
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
 
 def primer_2():
     print('primer2')
@@ -210,33 +216,37 @@ def primer_4():
     A,B,C su neotvorena polja, a brojevi oznacavaju broj mina u okolnim poljima.
     Zapisati u iskaznoj logici uslove koji moraju da vaze.
     '''
-    print('primer4')
     A,B,C = vars("A,B,C")
-    formula = (A | B) & (A | B) & ~(A & B) & ~(A & B) & (~A | ~B | ~C)\
-    & ~(A & B) & (~A | ~B | ~C)\
-    & ~(~A & ~B)\
-    & ~(~A & ~C)\
-    & ~(~B & ~C)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
+    formula = (A | B) & ~(A & B) \
+        & (B | A) & ~(B & A) \
+        & ~(~A & ~B & ~C)\
+        & (A | B)\
+        & (B | C)\
+        & (A | C)\
+        & ~(A & B & C)
+    print(formula)
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 
 def primer_5():
     '''
     Date su dve kutije A,B robot mora da stavi objekat u tacno jednu od njih.
     '''
-    A,B = vars("A,B")
-    formula = ~(A & B) & ~(~A & ~B) & (A | B)
-    print(primer_5.__doc__)
-    print(formula)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
+    A, B = vars("A,B")
+    formula = (A | B) & ~(A & B) & ~(~A & ~B)
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 
 def primer_6():
@@ -248,28 +258,27 @@ def primer_6():
     '''
     A,B,C,D = vars("A,B,C,D")
     formula = (A | B) & (C | D) & ~(A & B) & ~(C & D)
-    print(primer_6.__doc__)
-    print(formula)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 def primer_7():
     '''
     U iskaznoj logici zapisati uslov da bitovi 3-bitnog polja moraju biti jednaki
     '''
     A,B,C = vars("A,B,C")
-    formula = (A == B) & (B == C) & (A == C)
-    print(primer_7.__doc__)
-
-    print(formula)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
+    formula = (A == B) & (B == C)
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 def primer_8():
     '''
@@ -285,31 +294,31 @@ def primer_8():
     '''
     A,B,C,D = vars("A,B,C,D")
     formula = (B | D) & ~(B & D) & (A | C) & ~(A & C)
-    print(primer_8.__doc__)
 
-    print(formula)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
-
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 def primer_9():
     '''
     U iskoznoj logici zapisati da je 4 bitna reprezentacija broja palindrom ali da 
     bitovi nisu jednaki
+    ABCD
     '''
+
     A,B,C,D = vars("A,B,C,D")
     formula = (A == D) & (B == C) & ~(A == B & B == C & C == D)
-    print(primer_9.__doc__)
-
-    print(formula)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 def primer_10():
     '''
@@ -317,16 +326,16 @@ def primer_10():
     Ukoliko je prvo crveno, druga dva moraju biti iste boje.
     Ukoliko je drugo crveno, trece mora biti plavo.
     '''
-    A,B,C = vars("A,B,C")
+    A,B,C = vars("A,B,C") # A:True => A crveno
     formula = (A >> (B == C)) & (B >> ~C)
-    print(primer_10.__doc__)
 
-    print(formula)
-    print("Tautology: ", formula.is_tautology())
-    print("Satisfiable: ", formula.is_satisfiable())
-    print("Falsifiable: ", formula.is_falsifiable())
-    print("Unsatisfiable: ", formula.is_unsatisfiable())
-    print('---------------------')
+    print("is_valid: ", formula.is_valid())
+    print("is_satisfiable: ", formula.is_satisfiable())
+    print("is_falsifiable: ", formula.is_falsifiable())
+    print("is_contradictory: ", formula.is_contradictory())
+    print("all true valuations: ")
+    for val in formula.all_valuations_that_interpret_to_true():
+        print(val)
 
 def primer_11():
     '''
